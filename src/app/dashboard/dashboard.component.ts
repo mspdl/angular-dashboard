25ec90fe942662636c3b155531c43e3c682f6029
Morgan <morgan.espindola@gmail.com>
2023-12-06 22:37:15 -0300

    Dashboard init method implementation

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
+
+declare var google: any;
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+  private data: any;
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.dataService.getData().subscribe((data) => {
+      this.data = data;
+      this.init();
+    });
+  }
+
+  init(): void {
+    if (typeof google !== 'undefined') {
+      google.charts.load('current', { packages: ['corechart'] });
+      setTimeout(() => {
+        google.charts.setOnLoadCallback(this.showCharts());
+      }, 1000);
+    }
+  }
+
+  showCharts(): void {}
+
+
+}
